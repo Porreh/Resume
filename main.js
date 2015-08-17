563d1324e2d7f33692714068ef22ddd65579eace
Porreh <porreh@gmail.com>
2015-08-17 08:27:58 +0300

    Initial Commit

---
+++ main.js
@@ -0,0 +1,41 @@
+var myAge = {
+  init: function() {
+    this.cacheDOM();
+    this.yearNow();
+    this.monthNow();
+		this.myAgeNow();
+		this.render();
+	},
+	
+	cacheDOM: function() {
+		this.placeholder = document.querySelector('bdi');
+	},
+	
+	yearNow: function() {
+	  var d = new Date();
+    var y = d.getFullYear();
+    return y;
+	},
+	
+	monthNow: function() {
+	  var d = new Date();
+    var m = d.getMonth();
+    return m;
+	},
+	
+	myAgeNow: function() {
+	  var y = this.yearNow();
+	  var m = this.monthNow();
+	  if (m < 12) {
+	    return (y - 1995) - 1;
+    } else {
+      return y - 1995;
+    }
+	},
+	
+	render: function() {
+	  this.placeholder.innerHTML = this.myAgeNow();
+	}
+};
+
+myAge.init();
