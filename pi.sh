ddab22182bd7705a2e3813fb47e7ea2691135a6c
Dmytro <Porreh@gmail.com>
2015-12-05 20:28:10 +0200

    Update pi.sh

--- pi.sh
+++ pi.sh
@@ -8,12 +8,17 @@ sudo apt-get dist-upgrade
 #         Add Repositories          #
 #...................................#
 
+wget -q -O - https://dl-ssl.google.com/linux/linux_signing_key.pub | sudo apt-key add - 
+
 sudo add-apt-repository "deb http://archive.canonical.com/ $(lsb_release -sc) partner"
+sudo add-apt-repository "deb http://dl.google.com/linux/chrome/deb/ stable main"
 sudo add-apt-repository ppa:elementary-os/daily
 sudo add-apt-repository ppa:mpstark/elementary-tweaks-daily
 sudo apt-add-repository ppa:ricotz/docky
 sudo add-apt-repository ppa:evolve-os/ppa
 sudo add-apt-repository ppa:nilarimogard/webupd8
+sudo add-apt-repository ppa:videolan/stable-daily
+sudo add-apt-repository ppa:transmissionbt/ppa
 
 sudo apt-get update
 
@@ -21,7 +26,7 @@ sudo apt-get update
 #         Install applications          #
 #.......................................#
 
-sudo apt-get install mdm ubuntu-mdm-themes budgie-desktop plank 
+sudo apt-get install vlc transmission atom ubuntu-restricted-extras google-chrome-stable
 
 #.......................................#
 #               Cleaning                #
